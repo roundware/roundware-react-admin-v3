de2c578c5738d01c4b2a7070d0de9eaff8bce1db
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-02 17:09:36 +0530

    theme

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const adminTheme = createMuiTheme({
+    palette: {
+		secondary: {
+			main:"#6b96d8"
+		},
+    },
+});
+
+
+export default adminTheme;
