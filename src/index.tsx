fd00db4b496364188ca8f841b53ace87f47ff6d8
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-02 17:08:36 +0530

    wrap with projects provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProjectsProvider } from "./providers/ProjectsContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProjectsProvider>
+      <App />
+    </ProjectsProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
