87b3324eb342db14931981dcdd85e56f63e8a166
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-02 17:08:28 +0530

    customized data provider to get projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
-import React from "react";
-import { Admin, Resource } from "react-admin";
-import drfProvider, { tokenAuthProvider, fetchJsonWithAuthToken } from "ra-data-django-rest-framework";
+import React, { useEffect } from "react";
+import { Admin, Resource, DataProvider } from "react-admin";
+import drfProvider, {
+  tokenAuthProvider,
+  fetchJsonWithAuthToken,
+} from "ra-data-django-rest-framework";
 import ProjectList from "./components/ProjectList";
 import ProjectCreate from "./components/ProjectCreate";
 import ProjectEdit from "./components/ProjectEdit";
 import AssetList from "./components/AssetList";
 import AssetCreate from "./components/AssetCreate";
 import AssetEdit from "./components/AssetEdit";
+import adminTheme from "./styles";
+import CustomLayout from "./components/Layout";
+import { IProject, useProjects } from "./providers/ProjectsContext";
 
-const authProvider = tokenAuthProvider({ "obtainAuthTokenUrl": `${process.env.REACT_APP_SERVER_URL}/api/2/login/` });
-const dataProvider = drfProvider(`${process.env.REACT_APP_SERVER_URL}/api/2`, fetchJsonWithAuthToken);
+const authProvider = tokenAuthProvider({
+  obtainAuthTokenUrl: `${process.env.REACT_APP_SERVER_URL}/api/2/login/`,
+});
+const dataProvider = drfProvider(
+  `${process.env.REACT_APP_SERVER_URL}/api/2`,
+  fetchJsonWithAuthToken
+);
+
+const customDataProvider: DataProvider = {
+  ...dataProvider,
+  getList: async (resource, params) => {
+    if (resource === 'projects') {
+      const {json}= await fetchJsonWithAuthToken(`${process.env.REACT_APP_SERVER_URL}/api/2/projects`, {});
+      return {
+        data: json,
+        total: json?.length
+      }
+    }
+    return dataProvider.getList(resource, params);
+  }
+} 
 
 function App() {
+
+  const { setProjectsList, projectsList, selectedProject } = useProjects();
+
+  useEffect(() => {
+  // @ts-ignore
+    customDataProvider.getList<IProject>(`projects`).then((project) => {
+    setProjectsList(project.data)
+  });
+}, [])
+
   return (
     <Admin
-      dataProvider={dataProvider}
+      theme={adminTheme}
+      layout={CustomLayout}
+      title="Roundware Admin"
+      dataProvider={customDataProvider}
       authProvider={authProvider}
     >
       <Resource
@@ -23,18 +61,22 @@ function App() {
         create={ProjectCreate}
         edit={ProjectEdit}
       />
+      {/* 
+      
+      
       <Resource
-        name="assets"
-        list={AssetList}
-        create={AssetCreate}
-        edit={AssetEdit}
-      />
-      <Resource name="tags" />
-      <Resource name="languages" />
-      <Resource name="localizedstrings" />
-      <Resource name="users" />
+          name="assets"
+          list={AssetList}
+          create={AssetCreate}
+          edit={AssetEdit}
+        />
+        <Resource name="tags" />
+        <Resource name="languages" />
+        <Resource name="localizedstrings" />
+        <Resource name="users" />
+       */}
     </Admin>
-  )
+  );
 }
 
 export default App;
