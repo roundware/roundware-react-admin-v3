47547785b5d50b4005df7a6dcdbb7423f58ee1ce
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-02 20:28:13 +0530

    card styling & redirection for projects

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -8,10 +8,17 @@ import {
   EditButton,
   DeleteButton,
   ListProps,
+  Show,
+  SimpleShowLayout,
+  CardActions
 } from "react-admin";
-import { useProjects } from "../providers/ProjectsContext";
-import { useAuthState, useDataProvider } from "ra-core";
-const ProjectList = (props: ListProps) => {
+import { IProject, useProjects } from "../providers/ProjectsContext";
+import { useAuthState, useDataProvider, useListContext, useRedirect } from "ra-core";
+import Card from '@material-ui/core/Card';
+import { CardActionArea, CardContent, CardMedia, Button, makeStyles, Typography, CardHeader, Grid } from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/DeleteRounded';
+const ProjectShow = (props: ListProps) => {
 
   const { setProjectsList, projectsList, selectedProject } = useProjects();
   const { authenticated } = useAuthState();
@@ -24,17 +31,92 @@ const ProjectList = (props: ListProps) => {
     })
   }, [authenticated]);
 
-
-  return <List {...props}>
-    <Datagrid>
-      <TextField source='id' />
-      <TextField source='name' />
-      <NumberField source='latitude' options={{ maximumFractionDigits: 8 }} />
-      <NumberField source='longitude' options={{ maximumFractionDigits: 8 }} />
-      <EditButton basePath="/projects" />
-      <DeleteButton basePath="/projects" />
-    </Datagrid>
+  return <List hasCreate={false} hasEdit={false} hasShow={false} bulkActionButtons={false} pagination={false} component={ProjectCardWrapper}  {...props} >
+    <ProjectCard />
   </List>
 }
 
-export default ProjectList;
+
+const useCardStyles = makeStyles(theme => ({
+  root: {
+    width: 275,
+    height: 160,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  createContent: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: "100%"
+  },
+  cardContent: {
+    height: "100%",
+    padding: theme.spacing(2)
+  }
+}));
+const ProjectCardWrapper = ({ children }: {children: React.ReactNode}) => {
+  return <Grid alignContent="center" justify="center" container spacing={2}>{children}</Grid>
+}
+const ProjectCard = () => {
+  const { data } = useListContext<IProject>();
+  const redirect = useRedirect();
+  const classes = useCardStyles();
+  const { selectProject } = useProjects();
+
+  const handleOnProjectSelect = (p: IProject) => {
+    selectProject(p);
+    redirect(`list`, `/`)
+  }
+  return (<>
+      <Grid item>
+      <CreateProjectCard />
+      </Grid>
+      {Array.isArray(Object.values(data)) && Object.values(data).map((p) => <Grid item><Card onClick={()=>handleOnProjectSelect(p)} key={p?.id} className={classes.root}>
+
+      <CardActionArea className={classes.cardContent}>
+        
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Project #{p?.id}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {p?.name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+            Created: {new Date(p?.pub_date).toLocaleString()}
+        </Typography>
+      
+      </CardActionArea>
+      
+    </Card></Grid>)}</>)
+  
+}
+
+const CreateProjectCard = () => {
+  const classes = useCardStyles();
+  return <Card className={classes.root}>
+    <CardActionArea className={classes.createContent}>
+      
+      
+        <AddIcon fontSize="large" />
+      
+      <Typography  variant="h6" component="h2">
+        Create new Project
+      </Typography>
+      
+      
+  </CardActionArea>
+  
+</Card>
+}
+
+export default ProjectShow;
