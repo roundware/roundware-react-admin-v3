9943b84d9e7477d6296363e24ff1e45a8fe9fde3
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-02 17:37:24 +0530

    added authenticated check

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect} from "react";
 import {
   List,
   Datagrid,
@@ -9,8 +9,22 @@ import {
   DeleteButton,
   ListProps,
 } from "react-admin";
-
+import { useProjects } from "../providers/ProjectsContext";
+import { useAuthState, useDataProvider } from "ra-core";
 const ProjectList = (props: ListProps) => {
+
+  const { setProjectsList, projectsList, selectedProject } = useProjects();
+  const { authenticated } = useAuthState();
+  const dataProvider = useDataProvider();
+  useEffect(() => {
+    if (!authenticated) return;
+  // @ts-ignore
+  dataProvider.getList<IProject>(`projects`).then((project) => {
+      setProjectsList(project.data)
+    })
+  }, [authenticated]);
+
+
   return <List {...props}>
     <Datagrid>
       <TextField source='id' />
