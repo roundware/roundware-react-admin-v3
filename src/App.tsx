2db4f6bf55183dbd190d53e23503ed71bf4f6003
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-04 11:18:05 +0530

    use custom date provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import React, { useEffect } from "react";
-import { Admin, Resource, DataProvider, GetListParams, GetManyReferenceParams, GetListResult } from "react-admin";
 import drfProvider, {
-  tokenAuthProvider,
-  fetchJsonWithAuthToken,
-  CustomDataProvider,
+  CustomDataProvider, fetchJsonWithAuthToken, tokenAuthProvider
 } from "ra-data-roundware-drf";
-import ProjectList from "./components/ProjectList";
-import ProjectCreate from "./components/ProjectCreate";
-import ProjectEdit from "./components/ProjectEdit";
-import AssetList from "./components/AssetList";
+import React from "react";
+import { Admin, Resource } from "react-admin";
 import AssetCreate from "./components/AssetCreate";
 import AssetEdit from "./components/AssetEdit";
-import adminTheme from "./styles";
+import AssetList from "./components/AssetList";
 import CustomLayout from "./components/Layout";
-import { IProject, useProjects } from "./providers/ProjectsContext";
 import Dashboard from "./components/Layout/Dashboard";
+import ProjectCreate from "./components/ProjectCreate";
+import ProjectEdit from "./components/ProjectEdit";
+import ProjectList from "./components/ProjectList";
+import { useProjects } from "./providers/ProjectsContext";
+import adminTheme from "./styles";
 
 const authProvider = tokenAuthProvider({
   obtainAuthTokenUrl: `${process.env.REACT_APP_SERVER_URL}/api/2/login/`,
@@ -30,6 +28,7 @@ const customDataProvider: CustomDataProvider = {
     if (resource === "assets") return await dataProvider.getList(resource, params, true);
     return await dataProvider.getList(resource, params);
   },
+
 };
 
 function App() {
