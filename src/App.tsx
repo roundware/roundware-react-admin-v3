7a960d554d16d5bd72698b164ba5ce0737dbf4dc
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-04 20:03:12 +0530

    remove data provider customization, aesthetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import drfProvider, {
-  CustomDataProvider, fetchJsonWithAuthToken, tokenAuthProvider
+  CustomDataProvider,
+  fetchJsonWithAuthToken,
+  tokenAuthProvider,
 } from "ra-data-roundware-drf";
 import React from "react";
-import { Admin, Resource } from "react-admin";
-import AssetCreate from "./components/AssetCreate";
-import AssetEdit from "./components/AssetEdit";
-import AssetList from "./components/AssetList";
+import {
+  Admin,
+  Resource,
+  ShowGuesser,
+  ListGuesser,
+  EditGuesser,
+} from "react-admin";
+import AssetCreate from "./components/Asset/AssetCreate";
+import AssetEdit from "./components/Asset/AssetEdit";
+import AssetList from "./components/Asset/AssetList";
 import CustomLayout from "./components/Layout";
 import Dashboard from "./components/Layout/Dashboard";
-import ProjectCreate from "./components/ProjectCreate";
-import ProjectEdit from "./components/ProjectEdit";
-import ProjectList from "./components/ProjectList";
+import ProjectCreate from "./components/Project/ProjectCreate";
+import ProjectEdit from "./components/Project/ProjectEdit";
+import ProjectList from "./components/Project/ProjectList";
 import { useProjects } from "./providers/ProjectsContext";
 import adminTheme from "./styles";
-
+import {
+  WebAsset,
+  Language,
+  Translate,
+  Label,
+  TagFaces,
+  PeopleAlt,
+} from "@material-ui/icons";
 const authProvider = tokenAuthProvider({
   obtainAuthTokenUrl: `${process.env.REACT_APP_SERVER_URL}/api/2/login/`,
 });
@@ -22,33 +37,22 @@ const dataProvider = drfProvider(
   fetchJsonWithAuthToken
 );
 
-const customDataProvider: CustomDataProvider = {
-  ...dataProvider,
-  getList: async (resource, params) => {
-    if (resource === "assets") return await dataProvider.getList(resource, params, true);
-    return await dataProvider.getList(resource, params);
-  },
-
-};
-
 function App() {
-  const { selectedProject } = useProjects();
-
   return (
     <Admin
       theme={adminTheme}
       layout={CustomLayout}
       title="Roundware Admin"
       // @ts-ignore
-      dataProvider={customDataProvider}
+      dataProvider={dataProvider}
       authProvider={authProvider}
-      dashboard={Dashboard}
     >
       <Resource
         name="projects"
         list={ProjectList}
         create={ProjectCreate}
         edit={ProjectEdit}
+        show={ShowGuesser}
       />
 
       <Resource
@@ -56,11 +60,39 @@ function App() {
         list={AssetList}
         create={AssetCreate}
         edit={AssetEdit}
+        options={{ label: "Assets" }}
+        icon={WebAsset}
       />
-      <Resource name="tags" />
-      <Resource name="languages" />
-      <Resource name="localizedstrings" />
-      <Resource name="users" />
+      <Resource
+        name="tagcategories"
+        options={{
+          label: "Tag Categories",
+        }}
+        list={ListGuesser}
+        edit={EditGuesser}
+        icon={Label}
+      />
+      <Resource
+        name="languages"
+        list={ListGuesser}
+        edit={EditGuesser}
+        icon={Language}
+      />
+      <Resource
+        name="localizedstrings"
+        options={{
+          label: "Localized Strings",
+        }}
+        list={ListGuesser}
+        edit={EditGuesser}
+        icon={Translate}
+      />
+      <Resource
+        name="users"
+        list={ListGuesser}
+        edit={EditGuesser}
+        icon={PeopleAlt}
+      />
     </Admin>
   );
 }
