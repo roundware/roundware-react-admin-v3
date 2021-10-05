bc3d9168fb9cffed3244679f060ed7b555df4f51
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-06 00:05:02 +0530

    layout changes for pie chart

--- src/components/Layout/Dashboard/DashboardContent.tsx
+++ src/components/Layout/Dashboard/DashboardContent.tsx
@@ -19,6 +19,7 @@ import { GetListResult, Record } from "react-admin";
 
 import SessionsChart from "./SessionsChart";
 import AssetsChart from "./AssetsChart";
+import ClientTypeChart from "./ClientTypeChart";
 
 interface Props {
   selectedProject: IProject;
@@ -33,8 +34,8 @@ const DashboardContent = (props: Props) => {
   return (
     <div>
       <Grid container spacing={4}>
-        <Grid container item spacing={3} xs={12}>
-          <Grid item md={3} xs={6}>
+        <Grid container item spacing={3} md={12} xs={12}>
+          <Grid item md={2} xs={6}>
             <CardWithIcon
               icon={Hearing}
               title="Total Listens"
@@ -43,7 +44,7 @@ const DashboardContent = (props: Props) => {
             />
           </Grid>
 
-          <Grid item md={3} xs={6}>
+          <Grid item md={2} xs={6}>
             <CardWithIcon
               icon={RecordVoiceOver}
               title="Total Recordings"
@@ -52,7 +53,7 @@ const DashboardContent = (props: Props) => {
             />
           </Grid>
 
-          <Grid item md={3} xs={6}>
+          <Grid item md={2} xs={12}>
             <CardWithIcon
               icon={WatchLater}
               title="Total Sessions"
@@ -60,21 +61,17 @@ const DashboardContent = (props: Props) => {
               to="/sessions"
             />
           </Grid>
-
-          <Grid item md={3} xs={6}>
-            <CardWithIcon
-              icon={Person}
-              title="Total Users"
-              subtitle={users?.total || 0}
-              to="/users"
-            />
+          <Grid container item xs={6} md={4}>
+            <div style={{ width: "100%", margin: 4 }}>
+              <Typography style={{ textAlign: "center" }}>
+                Types of Clients
+              </Typography>
+            </div>
+            <ClientTypeChart sessions={sessions} />
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
 
-        <Grid container item xs={12} spacing={3}>
+        <Grid container item xs={12} md={12} spacing={3}>
           <Grid item xs={12} md={6}>
             <SessionsChart sessions={sessions} />
           </Grid>
