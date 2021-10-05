057ebf109cc10cf6a86fda08ce1faac79fdb77a3
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-05 21:30:40 +0530

    initial create, edit list for speakers

---
+++ src/components/Speaker.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  CreateProps,
+  ListProps,
+  EditProps,
+  List,
+  Create,
+  Edit,
+  Datagrid,
+  SimpleForm,
+  TextInput,
+} from "react-admin";
+
+export const SpeakerList = (props: ListProps) => {
+  return (
+    <List {...props}>
+      <Datagrid></Datagrid>
+    </List>
+  );
+};
+
+export const SpeakerEdit = (props: EditProps) => {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="id" required />
+      </SimpleForm>
+    </Edit>
+  );
+};
+
+export const SpeakerCreate = (props: CreateProps) => {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput source="id" required />
+      </SimpleForm>
+    </Create>
+  );
+};
