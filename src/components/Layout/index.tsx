f7bf1b126f43e18d3f8c55b9b906e80770e7bae6
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-05 21:35:43 +0530

    redirect to dashboard instead to view analytics

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -93,7 +93,7 @@ const CustomLayout = (props: LayoutProps) => {
   const redirect = useRedirect();
   useEffect(() => {
     if (!selectedProject) redirect(`list`, `/projects`);
-    else redirect(`/projects/${selectedProject.id}/show`);
+    else redirect(`/`);
   }, [selectedProject]);
   return (
     <Layout
