98b1b5cd3ae7f416d25a50d31fbcb12276c76bd4
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-28 17:16:36 +0530

    custom slider for reusablility

---
+++ src/components/common/CustomSlider.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { Grid, Typography, withStyles, Slider } from "@material-ui/core";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import useFieldValue from "../../hooks/useFieldValue";
+interface Props {
+  field: string;
+  label: string;
+  defaultValue?: number;
+  vertical?: boolean;
+  icon?: React.ReactNode;
+}
+
+const CustomSlider = ({
+  field,
+  label,
+  vertical,
+  defaultValue = 100,
+  icon,
+}: Props) => {
+  const [value, setValue] = useFieldValue(field);
+  React.useEffect(() => {
+    setValue(value || defaultValue);
+  }, []);
+  return (
+    <Grid
+      container
+      direction="column"
+      spacing={1}
+      alignItems="center"
+      style={{ height: "100%" }}
+    >
+      <Grid item>
+        <Typography>{label}</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="subtitle1">{Number(value)?.toFixed(0)}</Typography>
+      </Grid>
+      <Grid item style={{ flexGrow: 1 }}>
+        <CustomSliderVariant
+          orientation={vertical ? "vertical" : `horizontal`}
+          valueLabelDisplay="off"
+          value={value}
+          onChange={(e, v) => setValue(v)}
+        />
+      </Grid>
+      <Grid item>{icon}</Grid>
+    </Grid>
+  );
+};
+
+export default CustomSlider;
+
+export const CustomSliderVariant = withStyles({
+  root: {
+    color: "#3f3dc0",
+    height: 8,
+    "&$vertical": {
+      width: 8,
+    },
+    marginLeft: -4,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover": {
+      boxShadow: "0px 0px 0px 8px rgba(84, 199, 97, 0.16)",
+    },
+    "&$active": {
+      boxShadow: "0px 0px 0px 12px rgba(84, 199, 97, 0.16)",
+    },
+  },
+  active: {
+    transform: "rotate(-90deg)",
+  },
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+    transform: "rotate(-90deg)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+  vertical: {
+    "& $rail": {
+      width: 8,
+    },
+    "& $track": {
+      width: 8,
+    },
+    "& $thumb": {
+      marginLeft: -8,
+      marginBottom: -11,
+    },
+  },
+})(Slider);
