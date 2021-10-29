d9b5bae366cbfca72ed6262053fa2111a326e753
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-29 21:44:44 +0530

    speaker list with selectable and aside component as map

---
+++ src/components/Speaker/SpeakerList.tsx
@@ -0,0 +1,71 @@
+import {
+  EditButton,
+  DeleteButton,
+  List,
+  Datagrid,
+  TextField,
+  BooleanField,
+  ListProps,
+  NumberField,
+  ReferenceField,
+  useRecordContext,
+  NumberInput,
+  TextInput,
+  SimpleForm,
+} from "react-admin";
+import { Grid, IconButton, Tooltip, Typography } from "@material-ui/core";
+import { useSpeakers } from "providers/SpeakersContext";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import { SpeakerEdit } from ".";
+import SpeakerShapesControl from "./SpeakerShapesControl";
+const SpeakerList = (props: ListProps) => {
+  return (
+    <>
+      <List
+        {...props}
+        aside={
+          <div style={{ width: "70%", margin: "0 18px" }}>
+            <SpeakerShapesControl />
+          </div>
+        }
+        title="Speakers"
+      >
+        <Datagrid rowClick="" expand={<SpeakerEdit />}>
+          <SpeakerHighter />
+
+          <TextField source="id" />
+          <BooleanField source="activeyn" label="Active" />
+          <TextField source="code" />
+
+          {/* <TextField source="backupuri" />
+      <TextField source="shape.type" />
+      <TextField source="boundary.type" /> */}
+
+          <EditButton basePath="/speakers" />
+          <DeleteButton basePath="/speakers" />
+        </Datagrid>
+      </List>
+    </>
+  );
+};
+
+const SpeakerHighter = () => {
+  const { id } = useRecordContext();
+  const { setSelectedSpeaker, selectedSpeaker } = useSpeakers();
+
+  const isSelected = id == selectedSpeaker;
+  return (
+    <Tooltip title={isSelected ? "Unselect" : `Select On Map`} placement="left">
+      <IconButton
+        onClick={() =>
+          setSelectedSpeaker((prev) => (prev == id ? null : Number(id)))
+        }
+      >
+        {isSelected ? <LocationOnIcon /> : <LocationOnOutlinedIcon />}
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default SpeakerList;
