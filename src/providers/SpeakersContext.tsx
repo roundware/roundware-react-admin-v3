dad88f0c03630550375884c7880f03d5f99fe532
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-30 22:03:09 +0530

    fetch speakers from data provider

--- src/providers/SpeakersContext.tsx
+++ src/providers/SpeakersContext.tsx
@@ -1,23 +1,45 @@
-import React, { useState } from "react";
-import { AllowChildrenOnlyProps } from "./ProjectsContext";
+import React, { useEffect, useMemo, useState } from "react";
+import { AllowChildrenOnlyProps, useProjects } from "./ProjectsContext";
 import { useRoundwareDataProvider } from "providers/DataProviderContext";
+import { ISpeaker } from "types/speaker";
+
 export interface ISpeakerContext {
   selectedSpeaker: number | null;
   setSelectedSpeaker: React.Dispatch<React.SetStateAction<number | null>>;
-  speakers?: [];
+  speakers?: ISpeaker[];
 }
 export const SpeakerContext = React.createContext<ISpeakerContext>(undefined!);
 export const useSpeakers = () => React.useContext(SpeakerContext);
 
 export const SpeakersProvider = ({ children }: AllowChildrenOnlyProps) => {
   const dataProvider = useRoundwareDataProvider();
+  const { selectedProject } = useProjects();
   const [selectedSpeaker, setSelectedSpeaker] = useState<number | null>(null);
+  const [speakers, setSpeakers] = useState<ISpeaker[]>();
+  useEffect(() => {
+    dataProvider
+      .getList(`speakers`, {
+        pagination: {
+          perPage: 0,
+          page: 0,
+        },
+        sort: {
+          field: "id",
+          order: "ASC",
+        },
+        filter: {
+          project_id: selectedProject?.id,
+        },
+      })
+      .then((res) => setSpeakers(res.data as ISpeaker[]));
+  }, [selectedProject?.id]);
 
   return (
     <SpeakerContext.Provider
       value={{
         selectedSpeaker,
         setSelectedSpeaker,
+        speakers,
       }}
     >
       {children}
