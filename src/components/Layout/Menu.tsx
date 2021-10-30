cc4bc9d0b41c9eaf0936106b25199e11b00b3699
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-30 22:02:26 +0530

    close sidebar on mouseleave

--- src/components/Layout/Menu.tsx
+++ src/components/Layout/Menu.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { createElement } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core";
 import {
   DashboardMenuItem,
@@ -8,6 +8,7 @@ import {
   MenuItemLink,
   getResources,
   MenuProps,
+  setSidebarVisibility,
 } from "react-admin";
 import DefaultIcon from "@material-ui/icons/ViewList";
 import LabelIcon from "@material-ui/icons/Label";
@@ -23,40 +24,47 @@ export const Menu = (props: MenuProps) => {
   const resources = useSelector(getResources);
   const classes = useStyles();
   const { selectedProject } = useProjects();
+  const dispatch = useDispatch();
+  const openMenu = () => dispatch(setSidebarVisibility(true));
+  const closeMenu = () => dispatch(setSidebarVisibility(false));
+
   return (
     <RAMenu {...props} className={classes.raMenu}>
-      {selectedProject && <DashboardMenuItem />}
-      {/* @ts-ignore */}
-      <MenuItemLink
-        key={"projects"}
-        to={{
-          pathname: `/${
-            selectedProject ? `projects/${selectedProject.id}/show` : `projects`
-          }`,
-        }}
-        primaryText={selectedProject ? `Project` : `All Projects`}
-        leftIcon={<AccountTree />}
-      />
-      {selectedProject &&
-        resources
-          .filter((resource) => resource.name !== "projects")
-          .sort((a, b) => (a.name > b.name ? 1 : -1))
-          .map((resource) => (
-            //   @ts-ignore
-            <MenuItemLink
-              key={resource.name}
-              to={{
-                pathname: `/${resource.name}`,
-              }}
-              primaryText={
-                (resource.options && resource.options.label) ||
-                resource.name.toString().charAt(0).toUpperCase() +
-                  resource.name.substr(1)
-              }
-              leftIcon={resource.icon ? <resource.icon /> : <DefaultIcon />}
-            />
-          ))}
-      {/* add your custom menus here */}
+      <div onMouseEnter={openMenu} onMouseLeave={closeMenu}>
+        {selectedProject && <DashboardMenuItem />}
+        {/* @ts-ignore */}
+        <MenuItemLink
+          key={"projects"}
+          to={{
+            pathname: `/${
+              selectedProject
+                ? `projects/${selectedProject.id}/show`
+                : `projects`
+            }`,
+          }}
+          primaryText={selectedProject ? `Project` : `All Projects`}
+          leftIcon={<AccountTree />}
+        />
+        {selectedProject &&
+          resources
+            .filter((resource) => resource.name !== "projects")
+            .sort((a, b) => (a.name > b.name ? 1 : -1))
+            .map((resource) => (
+              //   @ts-ignore
+              <MenuItemLink
+                key={resource.name}
+                to={{
+                  pathname: `/${resource.name}`,
+                }}
+                primaryText={
+                  (resource.options && resource.options.label) ||
+                  resource.name.toString().charAt(0).toUpperCase() +
+                    resource.name.substr(1)
+                }
+                leftIcon={resource.icon ? <resource.icon /> : <DefaultIcon />}
+              />
+            ))}
+      </div>
     </RAMenu>
   );
 };
