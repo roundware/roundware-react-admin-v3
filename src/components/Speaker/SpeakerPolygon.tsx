8c266f1cc25650de3a78b6423c5cfbdb6bf07e33
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-31 19:16:19 +0530

    added dotted line for previous polygon

--- src/components/Speaker/SpeakerPolygon.tsx
+++ src/components/Speaker/SpeakerPolygon.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect, useRef } from "react";
 import { ISpeaker } from "types/speaker";
 import {
   Polygon,
   PolygonProps,
   InfoWindow,
   useGoogleMap,
+  Polyline,
+  PolylineProps,
 } from "@react-google-maps/api";
 import {
   Toolbar,
@@ -17,9 +19,12 @@ import {
 import { polygonToGoogleMapPaths } from "utilities";
 import DeleteIcon from "@material-ui/icons/Delete";
 import SaveIcon from "@material-ui/icons/Save";
+import HistoryIcon from "@material-ui/icons/History";
 import { useSpeakers } from "providers/SpeakersContext";
 import buffer from "@turf/buffer";
 import MapControl from "components/common/MapControl";
+import { multiPolygon } from "@turf/helpers";
+import { useRoundwareDataProvider } from "providers/DataProviderContext";
 interface Props {
   speaker: ISpeaker;
 }
@@ -32,10 +37,13 @@ const SpeakerPolygonsGroup = ({ speaker }: Props) => {
   const map = useGoogleMap();
   const [shape, setShape] = useState(speaker.shape);
   const [distance, setDistance] = useState(speaker.attenuation_distance);
-  // the outer shape
+
+  const dataProvider = useRoundwareDataProvider();
+
+  // the editable shape
   const shapePolygonOptions: PolygonProps[`options`] = {
     fillColor: "lightblue",
-    fillOpacity: 1,
+    fillOpacity: 0.8,
     strokeColor: "red",
     strokeOpacity: 1,
     strokeWeight: 2,
@@ -43,19 +51,7 @@ const SpeakerPolygonsGroup = ({ speaker }: Props) => {
     draggable: isSelected,
     editable: isSelected,
     geodesic: false,
-    zIndex: 1,
-  };
-
-  const attenuationBorderOptions = {
-    fillOpacity: 0,
-    strokeColor: "#00000050",
-    strokeOpacity: 1,
-    strokeWeight: 1,
-    clickable: true,
-    draggable: false,
-    editable: false,
-    geodesic: false,
-    zIndex: 2,
+    zIndex: 3,
   };
 
   // the inner border, should not be editable
@@ -70,12 +66,83 @@ const SpeakerPolygonsGroup = ({ speaker }: Props) => {
     return polygonToGoogleMapPaths(shape);
   }, [shape]);
 
+  const [dragging, setDragging] = useState(false);
+
+  const handleDragStart = () => setDragging(true);
+  const updatePolygon = (e: google.maps.MapMouseEvent) => {
+    setDragging(false);
+    const newPath = polygon
+      ?.getPath()
+      .getArray()
+      .map((p) => [p.lng(), p.lat()]);
+
+    if (Array.isArray(newPath)) {
+      const newMultiPolygon = multiPolygon([[newPath]]).geometry;
+      setShape(newMultiPolygon);
+    }
+
+    // if (Array.isArray(newPath)) {
+    //   setShape(multiPolygon(newPath.map(p => p.))
+    // }
+  };
+  const [polygon, setPolygon] = useState<google.maps.Polygon>();
+  const handleOnPolygonLoad = (loadedPolygon: google.maps.Polygon) =>
+    setPolygon(loadedPolygon);
+
+  const handleSave = () => {
+    dataProvider.update(`speakers`, {
+      id: speaker.id,
+      data: {
+        ...speaker,
+        shape,
+      },
+      previousData: {
+        ...speaker,
+      },
+    });
+  };
+
+  const polylineOptions = {
+    strokeColor: "#000000",
+    strokeOpacity: 0.1,
+    icons: [
+      {
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillOpacity: 0.5,
+          scale: 1,
+        },
+        offset: "0",
+        repeat: "10px",
+      },
+    ],
+  };
+
+  const handleDiscard = () => setShape(speaker.shape);
+
+  const handleDelete = () => {};
   return (
     <div>
-      <Polygon paths={shapePath} options={shapePolygonOptions} />
+      {/* original shape */}
+      <Polyline
+        path={polygonToGoogleMapPaths(speaker.shape)}
+        draggable={false}
+        options={polylineOptions}
+      />
+      {/* editable shape */}
+      <Polygon
+        paths={shapePath}
+        onDragStart={handleDragStart}
+        // onDragEnd={updatePolygon}
+        onMouseUp={updatePolygon}
+        onLoad={handleOnPolygonLoad}
+        options={shapePolygonOptions}
+      />
+
       <Polygon
         paths={attenuationBorderPath}
         options={attenuationBorderOptions}
+        visible={!dragging}
       />
 
       {isSelected && (
@@ -84,14 +151,21 @@ const SpeakerPolygonsGroup = ({ speaker }: Props) => {
             <Grid direction="column" spacing={1}>
               <Grid item>
                 <Tooltip title="Save Changes" placement="right">
-                  <IconButton>
+                  <IconButton onClick={handleSave}>
                     <SaveIcon />
                   </IconButton>
                 </Tooltip>
               </Grid>
               <Grid item>
                 <Tooltip title="Discard Changes" placement="right">
-                  <IconButton>
+                  <IconButton onClick={handleDiscard}>
+                    <HistoryIcon />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Delete Shape" placement="right">
+                  <IconButton onClick={handleDelete}>
                     <DeleteIcon />
                   </IconButton>
                 </Tooltip>
@@ -105,3 +179,15 @@ const SpeakerPolygonsGroup = ({ speaker }: Props) => {
 };
 
 export default SpeakerPolygonsGroup;
+
+const attenuationBorderOptions = {
+  fillOpacity: 0,
+  strokeColor: "#000000",
+  strokeOpacity: 1,
+  strokeWeight: 1,
+  clickable: true,
+  draggable: false,
+  editable: false,
+  geodesic: false,
+  zIndex: 2,
+};
