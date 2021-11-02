8fbad4b8bccaedf819f46f59726e46dc95e77b2c
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-11-02 22:42:02 +0530

    create spekaer

--- src/components/Speaker/index.tsx
+++ src/components/Speaker/index.tsx
@@ -10,10 +10,13 @@ import {
   SelectInput,
   SimpleForm,
   TextInput,
+  Record,
 } from "react-admin";
+import { useProjects } from "providers/ProjectsContext";
 import SpeakerAudioControls from "./SpeakerAudioControls";
 
 export const SpeakerEdit = (props: EditProps) => {
+  const { selectedProject } = useProjects();
   return (
     <Edit {...props}>
       <SimpleForm>
@@ -23,11 +26,14 @@ export const SpeakerEdit = (props: EditProps) => {
         <SpeakerAudioControls />
         <TextInput source="uri" fullWidth />
         <TextInput source="backupuri" fullWidth />
-        <TextInput source="shape.type" fullWidth />
-        <TextInput source="boundary.type" fullWidth />
-        <NumberInput source="attenuation_distance" fullWidth />
-        <TextInput source="attenuation_border.type" fullWidth />
-        <ReferenceInput source="project_id" reference="projects">
+
+        <NumberInput source="attenuation_distance" required fullWidth />
+
+        <ReferenceInput
+          source="project_id"
+          defaultValue={selectedProject!.id}
+          reference="projects"
+        >
           <SelectInput optionText="name" fullWidth />
         </ReferenceInput>
       </SimpleForm>
@@ -36,21 +42,28 @@ export const SpeakerEdit = (props: EditProps) => {
 };
 
 export const SpeakerCreate = (props: CreateProps) => {
+  const { selectedProject } = useProjects();
+  const transform = (data: Record) => {
+    data.project = selectedProject!.id;
+    if (typeof data?.file?.src == "string") {
+      data.file = data.file.rawFile;
+      delete data.uri;
+      delete data.backupuri;
+    }
+    return data;
+  };
   return (
-    <Create {...props}>
+    <Create {...props} transform={transform}>
       <SimpleForm>
-        <TextInput source="id" disabled />
-        <BooleanInput source="activeyn" fullWidth />
+        <BooleanInput source="activeyn" fullWidth defaultChecked />
         <TextInput source="code" fullWidth required />
-        {/* <SpeakerAudioControls /> */}
-        <TextInput source="uri" fullWidth required />
-        <TextInput source="backupuri" fullWidth />
 
-        <NumberInput source="attenuation_distance" fullWidth />
-        <TextInput source="attenuation_border.type" fullWidth />
-        <ReferenceInput source="project_id" reference="projects">
-          <SelectInput optionText="name" fullWidth />
-        </ReferenceInput>
+        <SpeakerAudioControls />
+        <NumberInput
+          source="attenuation_distance"
+          fullWidth
+          helperText="Meters"
+        />
       </SimpleForm>
     </Create>
   );
