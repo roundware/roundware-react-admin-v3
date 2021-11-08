aa8b9626c12c81515ad507e06acb080455178504
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-11-08 20:36:58 +0530

    layout fixes

--- src/components/Speaker/SpeakerList.tsx
+++ src/components/Speaker/SpeakerList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconButton, Tooltip } from "@material-ui/core";
+import { IconButton, Tooltip, Grid, Paper } from "@material-ui/core";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import { useSpeakers } from "providers/SpeakersContext";
@@ -17,32 +17,45 @@ import { SpeakerEdit } from ".";
 import SpeakerShapesControl from "./SpeakerShapesControl";
 const SpeakerList = (props: ListProps): JSX.Element => {
   return (
-    <>
-      <List
-        {...props}
-        aside={
-          <div style={{ width: "60%", margin: "0 18px" }}>
-            <SpeakerShapesControl />
-          </div>
-        }
-        title="Speakers"
-      >
-        <Datagrid expand={<SpeakerEdit />} style={{ flexShrink: 1 }}>
-          <SpeakerHighter />
+    <Grid
+      spacing={3}
+      container
+      direction="row"
+      wrap="nowrap"
+      style={{ marginTop: 28 }}
+      component={Paper}
+    >
+      <Grid item xs={12} md={6} sm={6} style={{ flexShrink: 1, flexGrow: 0 }}>
+        <List
+          {...props}
+          title="Speakers"
+          component="div"
+          bulkActionButtons={false}
+        >
+          <Datagrid
+            expand={<SpeakerEdit />}
+            hasBulkActions={false}
+            style={{ flexShrink: 1 }}
+          >
+            <SpeakerHighter />
 
-          <TextField source="id" />
-          <BooleanField source="activeyn" label="Active" />
-          <TextField source="code" />
+            <TextField source="id" />
+            <BooleanField source="activeyn" label="Active" />
+            <TextField source="code" />
 
-          {/* <TextField source="backupuri" />
+            {/* <TextField source="backupuri" />
       <TextField source="shape.type" />
       <TextField source="boundary.type" /> */}
 
-          <EditButton basePath="/speakers" />
-          <DeleteButton basePath="/speakers" />
-        </Datagrid>
-      </List>
-    </>
+            <EditButton label="" style={{ margin: 0 }} basePath="/speakers" />
+            <DeleteButton label="" basePath="/speakers" />
+          </Datagrid>
+        </List>
+      </Grid>
+      <Grid xs={12} style={{ flexShrink: 1, flexGrow: 1 }}>
+        <SpeakerShapesControl />
+      </Grid>
+    </Grid>
   );
 };
 
