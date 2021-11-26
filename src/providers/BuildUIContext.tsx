eb88100026b9c7b19eb4df4725312c2a5defa213
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-11-26 19:04:27 +0530

    put items tree in context

--- src/providers/BuildUIContext.tsx
+++ src/providers/BuildUIContext.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { IUIGroup } from "types/uiGroups";
+import { IUIGroup, UiItemNode } from "types/uiGroups";
 import { useRoundwareDataProvider } from "./DataProviderContext";
 import { AllowChildrenOnlyProps, useProjects } from "./ProjectsContext";
 
@@ -8,6 +8,9 @@ export interface IBuildUIContext {
   uiMode: IUIGroup[`ui_mode`];
   setUiMode: React.Dispatch<React.SetStateAction<IUIGroup[`ui_mode`]>>;
   refetchData: () => void;
+  uiItemsTree: UiItemNode[];
+  uiItemsList: UiItemNode[];
+  loading: boolean;
 }
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const BuildUIContext = React.createContext<IBuildUIContext>(undefined!);
@@ -31,6 +34,39 @@ export const BuildUIContextProvider = ({
   useEffect(() => {
     setUiGroups(fetchedData.filter((g) => g.ui_mode == uiMode));
   }, [uiMode, fetchedData]);
+
+  const [uiItemsTree, setUiItemsTree] = useState<UiItemNode[]>([]);
+  const [uiItemsList, setUiItemsList] = useState<UiItemNode[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const uiItems: UiItemNode[] = [];
+    const promises: Promise<void>[] = [];
+    uiGroups.forEach((g) => {
+      g.ui_items.forEach((item) => {
+        const prom = dataProvider
+          .getOne(`tags`, {
+            id: item.tag_id,
+          })
+          .then((res) => {
+            uiItems.push({
+              id: item.id,
+              parent_id: item.parent_id,
+              index: item.index,
+              displayText: res.data.value,
+              ui_group_id: item.ui_group_id,
+            });
+          });
+        promises.push(prom);
+      });
+    });
+    Promise.all(promises).then(() => {
+      setUiItemsList(uiItems);
+      setUiItemsTree(list_to_tree(uiItems));
+      setLoading(false);
+    });
+  }, [uiGroups]);
 
   const refetchData = useCallback(() => {
     dataProvider
@@ -57,9 +93,37 @@ export const BuildUIContextProvider = ({
         uiMode,
         setUiMode,
         refetchData,
+        uiItemsTree,
+        uiItemsList,
+        loading,
       }}
     >
       {children}
     </BuildUIContext.Provider>
   );
 };
+
+function list_to_tree(list: UiItemNode[]) {
+  // eslint-disable-next-line prefer-const
+  let map: {
+    [index: number]: number;
+  } = {};
+  let node: UiItemNode, i: number;
+
+  const roots: UiItemNode[] = [];
+  for (i = 0; i < list.length; i += 1) {
+    map[list[i].id] = i; // initialize the map
+    list[i].children = []; // initialize the children
+  }
+
+  for (i = 0; i < list.length; i += 1) {
+    node = list[i];
+    if (node.parent_id !== null) {
+      // if you have dangling branches check that map[node.parentId] exists
+      list[map[node?.parent_id]]?.children?.push(node);
+    } else {
+      roots.push(node);
+    }
+  }
+  return roots;
+}
