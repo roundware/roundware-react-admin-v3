c613ca33d339f1cd64682112b2d68b6e118bf078
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-12-01 20:35:30 +0530

    language type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,11 @@ export interface Coordinates {
     latitude: number;
     longitude: number;
 }
+
+export interface ILanguage {
+    id: number;
+    name: string;
+    language_code: string;
+}
+
+
