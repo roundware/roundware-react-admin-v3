286da12dbe7b895feace432c79432abd009fd2c7
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-12-02 17:42:11 +0530

    use localizedstring type instead

--- src/types/tags.ts
+++ src/types/tags.ts
@@ -1,23 +1,29 @@
+import { LocalizedString } from "types";
+
 export interface ITag {
-    id:number,
-      value: string,
-      description: string,
-      data: string,
-      filter: string,
-      location: null,
-      project_id: number,
-      tag_category_id:number,
-      description_loc: null,
-      msg_loc: string,
-      relationships: {
-          id: number,
-          tag_id:number,
-          parent_id: null
-        }[]
+  id: number;
+  value: string;
+  description: string;
+  data: string;
+  filter: string;
+  location: null;
+  project_id: number;
+  tag_category_id: number;
+  loc_description?: string | null | number[];
+  loc_msg?: number[];
+  msg_loc: string;
+  loc_msg_admin?: LocalizedString[];
+  loc_description_admin?: LocalizedString[];
+
+  relationships: {
+    id: number;
+    tag_id: number;
+    parent_id: null;
+  }[];
 }
 
 export interface ITagCategory {
-    id: number,
-    name: string,
-    data: string
+  id: number;
+  name: string;
+  data: string;
 }
