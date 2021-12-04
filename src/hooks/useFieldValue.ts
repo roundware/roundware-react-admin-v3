25eefe2efd05180b9dcdaec35b675ce4864a5dd8
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-12-04 19:08:49 +0530

    added default value support

--- src/hooks/useFieldValue.ts
+++ src/hooks/useFieldValue.ts
@@ -1,7 +1,10 @@
 import { useField } from 'react-final-form';
 
-const useFieldValue = <T>(fieldName: string): [T, (newValue: T) => void] => {
-    const { input: { value, onChange: setValue}} = useField(fieldName);
+const useFieldValue = <T>(fieldName: string, defaultValue?: T): [T, (newValue: T) => void] => {
+    const { input: { value, onChange: setValue } } = useField(fieldName, {
+        defaultValue,
+        
+    });
     return [value, setValue];
 }
 
