5505acea17386fb3ec47ca2cda9f3f748bbcdf0a
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-12-04 19:08:34 +0530

    audio track list, edit,create

--- src/components/AudioTrack.tsx
+++ src/components/AudioTrack.tsx
@@ -1,41 +1,148 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import {
-  CreateProps,
-  ListProps,
-  EditProps,
-  List,
+  BooleanField,
+  BooleanInput,
   Create,
-  Edit,
+  CreateProps,
   Datagrid,
+  Edit,
+  EditProps,
+  FieldProps,
+  List,
+  ListProps,
   SimpleForm,
   TextInput,
 } from "react-admin";
-import { useProjects } from "../providers/ProjectsContext";
-
-export const AudioTrackList = (props: ListProps) => {
-  const { selectedProject } = useProjects();
+import RangeSlider from "./common/RangeSlider";
+export const AudioTrackList = (props: ListProps): JSX.Element => {
   return (
-    <List {...props} filter={{ project_id: selectedProject?.id }}>
-      <Datagrid></Datagrid>
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <RangeDisplay source="volume" label="Volume" />
+        <RangeDisplay source="duration" label="Duration" />
+        <RangeDisplay source="deadair" label="Dead Air" />
+        <RangeDisplay source="fadeintime" label="Fade In Time" />
+        <RangeDisplay source="fadeouttime" label="Fade Out Time" />
+        <RangeDisplay source="panpos" label="Pan Position" />
+        <RangeDisplay source="panduration" label="Pan Duration" />
+
+        <BooleanField source="repeatrecordings" label="Repeat Recordings" />
+        <BooleanField source="start_with_silence" label="Start With Silence" />
+        <BooleanField
+          source="fadeout_when_filtered"
+          label="Fade Out When Filtered"
+        />
+      </Datagrid>
     </List>
   );
 };
-
-export const AudioTrackEdit = (props: EditProps) => {
+const RangeDisplay = ({ record, source }: FieldProps) => {
+  const min = record?.[`min${source}`];
+  const max = record?.[`max${source}`];
+  return (
+    <Typography>
+      {min} - {max}
+    </Typography>
+  );
+};
+export const AudioTrackEdit = (props: EditProps): JSX.Element => {
   return (
     <Edit {...props}>
       <SimpleForm>
-        <TextInput source="id" required />
+        <TextInput source="id" required disabled />
+        <RangeSlider
+          source="volume"
+          label="Volume"
+          min={0}
+          max={1}
+          step={0.1}
+        />
+        <RangeSlider source="duration" unit="s" label="Duration" min={0} />
+        <RangeSlider source="deadair" label="Dead Air" min={0} />
+        <RangeSlider source="fadeintime" unit="s" label="Fade In Time" />
+        <RangeSlider source="fadeouttime" unit="s" label="Fade Out Time" />
+        <RangeSlider
+          source="panpos"
+          label="Pan Position"
+          min={-1}
+          max={1}
+          step={0.1}
+        />
+        <RangeSlider source="panduration" unit="s" label="Pan Duration" />
+        <BooleanInput source="repeatrecordings" label="Repeat Recordings" />
+        <BooleanInput source="start_with_silence" label="Start With Silence" />
+        <BooleanInput
+          source="fadeout_when_filtered"
+          label="Fade Out When Filtered"
+        />
       </SimpleForm>
     </Edit>
   );
 };
 
-export const AudioTrackCreate = (props: CreateProps) => {
+export const AudioTrackCreate = (props: CreateProps): JSX.Element => {
   return (
     <Create {...props}>
       <SimpleForm>
-        <TextInput source="id" required />
+        <RangeSlider
+          source="volume"
+          label="Volume"
+          min={0}
+          max={1}
+          step={0.1}
+          defaultValue={[0, 1]}
+        />
+        <RangeSlider
+          source="duration"
+          unit="s"
+          label="Duration"
+          step={0.1}
+          min={0}
+          defaultValue={[0, 100]}
+        />
+        <RangeSlider
+          source="deadair"
+          label="Dead Air"
+          step={0.1}
+          min={0}
+          defaultValue={[0, 50]}
+        />
+        <RangeSlider
+          source="fadeintime"
+          step={0.1}
+          unit="s"
+          label="Fade In Time"
+          defaultValue={[0, 100]}
+        />
+        <RangeSlider
+          source="fadeouttime"
+          step={0.1}
+          unit="s"
+          label="Fade Out Time"
+          defaultValue={[0, 100]}
+        />
+        <RangeSlider
+          source="panpos"
+          label="Pan Position"
+          min={-1}
+          max={1}
+          step={0.1}
+          defaultValue={[-0.1, 0.1]}
+        />
+        <RangeSlider
+          source="panduration"
+          unit="s"
+          label="Pan Duration"
+          step={0.1}
+          defaultValue={[0, 100]}
+        />
+        <BooleanInput source="repeatrecordings" label="Repeat Recordings" />
+        <BooleanInput source="start_with_silence" label="Start With Silence" />
+        <BooleanInput
+          source="fadeout_when_filtered"
+          label="Fade Out When Filtered"
+        />
       </SimpleForm>
     </Create>
   );
