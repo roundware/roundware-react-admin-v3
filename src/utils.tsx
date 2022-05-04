504b5d452f89cb1cc19b8fd55a1c3443fed6879c
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-05-04 19:50:37 +0530

    single logic to handle localized strings

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,35 @@
-export const dateFormatter = (v: string) => {
+import { RoundwareDataProvider } from "ra-data-roundware-drf";
+import { LocalizedString } from "types";
+import { Record } from "react-admin";
+export const dateFormatter = (v: string): string | undefined => {
   if (!v) return;
   return new Date(v).toISOString();
+};
+
+export const handleLocalizedStrings = async (
+  messages: LocalizedString[],
+  dataProvider: RoundwareDataProvider
+): Promise<number[]> => {
+  // update, delete or create the localized string
+  // if empty text then just delete
+  // if no id then create
+  // else update
+  const promises = messages.map((m) =>
+    dataProvider[m.id ? (m.text ? `update` : `delete`) : `create`](
+      `localizedstrings`,
+      {
+        id: m.id as Record[`id`],
+        data: m,
+        previousData: m as Record,
+      }
+    )
+  );
+
+  // execute requests  in parallel
+  const responses = await Promise.all(promises);
+
+  // filter ids that need to be deleted and sent what was create dor updated
+  return responses
+    .filter((r) => !messages.some((m) => m.text == "" && m.id == r.data.id))
+    .map((r) => parseInt(r.data.id.toString()));
 };
