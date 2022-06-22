bf74ce8b4a6e27195c573f595f2a0429f2d396a2
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-06-22 14:49:12 +0000

    import button

---
+++ src/components/common/ImportButton.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  DialogTitle,
+  IconButton,
+  Stack,
+} from "@mui/material";
+import UploadIcon from "@mui/icons-material/Upload";
+import CloseIcon from "@mui/icons-material/Close";
+import FullScreenIcon from "@mui/icons-material/Fullscreen";
+import { ListContextProvider, TextField, Datagrid } from "react-admin";
+import ImportView from "./ImportView";
+import useBoolean from "hooks/useBoolean";
+
+const ImportButton = () => {
+  const [open, setOpen] = useState(false);
+  const fullScreen = useBoolean();
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(true)}
+        size="small"
+        startIcon={<UploadIcon />}
+      >
+        Import
+      </Button>
+      {open && (
+        <Dialog open={open} fullScreen={fullScreen.value}>
+          <DialogTitle>
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <span>Import</span>
+              <Stack direction="row" spacing={1}>
+                <IconButton title="Full Screen" onClick={fullScreen.toggle}>
+                  <FullScreenIcon />
+                </IconButton>
+                <IconButton title="Close" onClick={() => setOpen(false)}>
+                  <CloseIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+          </DialogTitle>
+          <ImportView />
+        </Dialog>
+      )}
+    </>
+  );
+};
+
+export default ImportButton;
