71ffa62c2cda4108ee3aa5a821b432b50b4c9e46
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-06-25 14:24:57 +0000

    hope this way it will download

--- src/components/common/FileDownloadButton.tsx
+++ src/components/common/FileDownloadButton.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@mui/material";
 import useFieldValue from "hooks/useFieldValue";
-import React from "react";
+import React, { useState } from "react";
 import DownloadIcon from "@mui/icons-material/Download";
+import { LoadingButton } from "@mui/lab";
+import { useLocation } from "react-router-dom";
 type Props = {
   source: string;
 };
@@ -9,25 +11,50 @@ type Props = {
 const FileDownloadButton = ({ source }: Props) => {
   const [file] = useFieldValue<string>(source);
   const [id] = useFieldValue<number>("id");
+  const location = useLocation();
+  const [downloadProgress, setDownloadProgress] = useState(0);
   if (typeof file != "string") return null;
   return (
-    <Button
+    <LoadingButton
+      loading={downloadProgress > 0}
       startIcon={<DownloadIcon />}
       onClick={() => {
-        const link = document.createElement("a");
-        link.target = `_self`;
-        link.download = id + "_" + file.substring(file.lastIndexOf("/") + 1);
-        link.href = file;
-        // link.target = "_blank";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        const request = new XMLHttpRequest();
+
+        request.responseType = "blob";
+        request.onprogress = (ev) =>
+          setDownloadProgress(ev.loaded / ev.total / 100);
+        request.onload = (ev) => {
+          try {
+            const blob = request.response;
+
+            const downloadUrl = URL.createObjectURL(blob);
+            const a = document.createElement(`a`);
+            a.href = downloadUrl;
+            a.download =
+              location.pathname.split(`/`).reverse()[1].slice(0, -1) +
+              "_" +
+              id +
+              "_" +
+              file.slice(file.lastIndexOf(`/`));
+            document.body.appendChild(a);
+            a.click();
+          } catch (e) {
+            console.error(e);
+          } finally {
+            setDownloadProgress(0);
+          }
+        };
+        request.open(`GET`, file);
+        request.send();
       }}
       variant="outlined"
       sx={{ my: 1 }}
     >
-      Download File
-    </Button>
+      {downloadProgress > 0
+        ? `Downloading... ${downloadProgress.toFixed(2)}%`
+        : `Download File`}
+    </LoadingButton>
   );
 };
 
