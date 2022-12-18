2dee89f35d057f5bba8d1c2ee542e1b5c7c63221
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-12-18 19:27:36 +0530

    date range filter

--- src/components/Layout/Dashboard/DashboardContent.tsx
+++ src/components/Layout/Dashboard/DashboardContent.tsx
@@ -1,5 +1,5 @@
 import { Hearing, RecordVoiceOver, WatchLater } from "@mui/icons-material";
-import { Grid } from "@mui/material";
+import { CircularProgress, Grid } from "@mui/material";
 import React from "react";
 import { GetListResult, RaRecord } from "react-admin";
 import { ResourceList } from "../../../App";
@@ -14,88 +14,105 @@ import AssetListensChart from "components/charts/AssetListensChart";
 import { IAsset } from "types/asset";
 import { IListenEvent } from "types/listenEvents";
 interface Props {
-  sessions: GetListResult<RaRecord> | null;
-  assets: GetListResult<RaRecord> | null;
-  listenEvents: GetListResult<RaRecord> | null;
-  ranges: { [resrouce: string]: string };
+    sessions: GetListResult<RaRecord> | null;
+    assets: GetListResult<RaRecord> | null;
+    listenEvents: GetListResult<RaRecord> | null;
+    ranges: { [resrouce: string]: string };
 }
 
 const DashboardContent = (props: Props) => {
-  const { ranges, assets, listenEvents, sessions } = props;
-  return (
-    <div>
-      <Grid container spacing={4}>
-        <Grid container item spacing={3} md={12} xs={12}>
-          <Grid item md={2} xs={6}>
-            <CardWithIcon
-              icon={Hearing}
-              title="Listens"
-              subtitle={
-                listenEvents === null ? `Loading..` : listenEvents?.total || `0`
-              }
-              to={ResourceList.includes(`listenevents`) && "listenevents"}
-              helperText={ranges[`listenEvents`]}
-            />
-          </Grid>
+    const { ranges, assets, listenEvents, sessions } = props;
+    return (
+        <div>
+            <Grid container spacing={4}>
+                <Grid container item spacing={3} md={12} xs={12}>
+                    <Grid item md={2} xs={6}>
+                        <CardWithIcon
+                            icon={Hearing}
+                            title="Listens"
+                            subtitle={
+                                listenEvents === null
+                                    ? `Loading..`
+                                    : listenEvents?.total || `0`
+                            }
+                            to={
+                                ResourceList.includes(`listenevents`) &&
+                                "listenevents"
+                            }
+                            helperText={ranges[`listenEvents`]}
+                        />
+                    </Grid>
 
-          <Grid item md={2} xs={6}>
-            <CardWithIcon
-              icon={RecordVoiceOver}
-              title="Recordings"
-              subtitle={assets === null ? `Loading..` : assets?.total || `0`}
-              to={ResourceList.includes(`assets`) && "assets"}
-              helperText={ranges[`assets`]}
-            />
-          </Grid>
+                    <Grid item md={2} xs={6}>
+                        <CardWithIcon
+                            icon={RecordVoiceOver}
+                            title="Recordings"
+                            subtitle={
+                                assets === null
+                                    ? `Loading..`
+                                    : assets?.total || `0`
+                            }
+                            to={ResourceList.includes(`assets`) && "assets"}
+                            helperText={ranges[`assets`]}
+                        />
+                    </Grid>
 
-          <Grid item md={2} xs={12}>
-            <CardWithIcon
-              icon={WatchLater}
-              title="Sessions"
-              subtitle={
-                sessions === null ? `Loading..` : sessions?.total || `0`
-              }
-              to={ResourceList.includes(`sessions`) && "sessions"}
-              helperText={ranges[`sessions`]}
-            />
-          </Grid>
-        </Grid>
+                    <Grid item md={2} xs={12}>
+                        <CardWithIcon
+                            icon={WatchLater}
+                            title="Sessions"
+                            subtitle={
+                                sessions === null
+                                    ? `Loading..`
+                                    : sessions?.total || `0`
+                            }
+                            to={ResourceList.includes(`sessions`) && "sessions"}
+                            helperText={ranges[`sessions`]}
+                        />
+                    </Grid>
+                </Grid>
 
-        <Grid container item xs={12} md={12} spacing={3}>
-          <Grid item xs={12} md={4} lg={4}>
-            <AssetMediaTypesChart assets={assets} />
-          </Grid>
+                <Grid container item xs={12} md={12} spacing={3}>
+                    <Grid item xs={12} md={4} lg={4}>
+                        <AssetMediaTypesChart assets={assets} />
+                    </Grid>
 
-          <Grid container item xs={4} md={4}>
-            <ClientTypeChart sessions={sessions} />
-          </Grid>
+                    <Grid container item xs={4} md={4}>
+                        <ClientTypeChart sessions={sessions} />
+                    </Grid>
 
-          <Grid container item xs={4} md={4}>
-            <BrowsersChart sessions={sessions} />
-          </Grid>
+                    <Grid container item xs={4} md={4}>
+                        <BrowsersChart sessions={sessions} />
+                    </Grid>
 
-          <Grid item xs={12} md={12} lg={12}>
-            <AssetsChart assets={assets} />
-          </Grid>
+                    <Grid item xs={12} md={12} lg={12}>
+                        <AssetsChart assets={assets} />
+                    </Grid>
 
-          <Grid item xs={12}>
-            <AssetListensChart
-              assets={assets as GetListResult<IAsset>}
-              listenEvents={listenEvents as GetListResult<IListenEvent>}
-            />
-          </Grid>
+                    <Grid item xs={12}>
+                        {assets && listenEvents ? (
+                            <AssetListensChart
+                                assets={assets as GetListResult<IAsset>}
+                                listenEvents={
+                                    listenEvents as GetListResult<IListenEvent>
+                                }
+                            />
+                        ) : (
+                            <CircularProgress />
+                        )}
+                    </Grid>
 
-          <Grid item xs={12} md={12} lg={12}>
-            <SessionsChart sessions={sessions} />
-          </Grid>
+                    <Grid item xs={12} md={12} lg={12}>
+                        <SessionsChart sessions={sessions} />
+                    </Grid>
 
-          <Grid item xs={12} md={12} lg={12}>
-            <ListenEventsChart events={listenEvents} />
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
-  );
+                    <Grid item xs={12} md={12} lg={12}>
+                        <ListenEventsChart events={listenEvents} />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default React.memo(DashboardContent);
--- src/components/charts/AssetListensChart.tsx
+++ src/components/charts/AssetListensChart.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+import { DatePicker } from "@mui/x-date-pickers";
 import {
     Card,
     CardContent,
     CardHeader,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
     Slider,
+    Stack,
+    TextField,
+    TextFieldProps,
     Typography,
 } from "@mui/material";
+import { subDays } from "date-fns";
+import { useChartData } from "hooks/useChartData";
 import { capitalize, uniqBy } from "lodash";
 import React, { useEffect, useMemo, useState } from "react";
 import { GetListResult, useRedirect } from "react-admin";
@@ -36,9 +46,63 @@ type BarChartData = {
 };
 
 const AssetListensChart = ({ assets, listenEvents }: Props): JSX.Element => {
+    const lowestDate = useMemo(() => {
+        const dates = listenEvents?.data?.map((event) =>
+            new Date(event?.start_time).getTime()
+        );
+        return dates?.length ? new Date(Math.min(...dates)) : new Date();
+    }, [listenEvents]);
+
+    const [customRange, setCustomRange] = useState(false);
+
+    const [rangeDropdownValue, setRangeDropdownValue] = useState<
+        string | number
+    >(30);
+    const [range, setRange] = useState<[Date, Date]>([
+        subDays(new Date(), +rangeDropdownValue),
+        new Date(),
+    ]);
+    const [, setDropdownRange] = useState<[Date, Date]>(range);
+
+    const handleOnSelectChange = (value: string | number) => {
+        setRangeDropdownValue(value);
+        if (value === "custom") {
+            setCustomRange(true);
+            return;
+        }
+        setCustomRange(false);
+
+        if (value === "total") {
+            const newRange = [lowestDate, new Date()] as [Date, Date];
+            setRange(newRange);
+            setDropdownRange(newRange);
+            return;
+        }
+
+        const newRange = [subDays(new Date(), Number(value)), new Date()] as [
+            Date,
+            Date
+        ];
+        setRange(newRange);
+        setDropdownRange(newRange);
+    };
+
+    useEffect(() => {
+        handleOnSelectChange(30);
+    }, [listenEvents, assets]);
+
+    const [startDate, setStartDate] = useState(lowestDate || new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    useEffect(() => {
+        setRange([startDate, endDate]);
+
+        setDropdownRange([startDate, endDate]);
+    }, [startDate, endDate]);
+
     const [assetViewIndex, setAssetViewIndex] = useState([
         0,
-        uniqBy(listenEvents.data, "asset_id").length,
+        uniqBy(listenEvents?.data, "asset_id").length,
     ]);
 
     const perAssetListens: BarChartData[] = useMemo(() => {
@@ -47,31 +111,93 @@ const AssetListensChart = ({ assets, listenEvents }: Props): JSX.Element => {
         return assets.data
             .map((asset) => ({
                 assetId: asset.id,
-                total: listenEvents.data.filter(
-                    (listen) => listen.asset_id == asset.id
-                ).length,
+                total: listenEvents.data
+                    .filter((l) => {
+                        const date = new Date(l.start_time);
+                        return date >= range[0] && date <= range[1];
+                    })
+                    .filter((listen) => listen.asset_id == asset.id).length,
             }))
             .filter((a) => a.total > 0)
             .sort((a) => a.assetId)
             .sort((a, b) => (a.total > b.total ? -1 : 1))
             .slice(assetViewIndex[0], assetViewIndex[1]);
-    }, [assets, listenEvents, assetViewIndex]);
+    }, [assets, listenEvents, assetViewIndex, range]);
 
     const redirect = useRedirect();
 
     return (
         <Card>
-            <CardHeader
-                title={
-                    <>
-                        <Typography variant="h5" style={{ flexGrow: 1 }}>
-                            Asset Listens
-                        </Typography>
-                    </>
-                }
-            />
-
             <CardContent>
+                <CardHeader
+                    title={
+                        <>
+                            <Typography variant="h5" style={{ flexGrow: 1 }}>
+                                Asset Listens
+                            </Typography>
+                        </>
+                    }
+                />
+
+                <Stack
+                    direction={"row"}
+                    mb={1}
+                    spacing={2}
+                    justifyContent="end"
+                >
+                    <div>
+                        <FormControl style={{ width: 150 }}>
+                            <InputLabel>Range</InputLabel>
+                            <Select
+                                value={rangeDropdownValue}
+                                onChange={(e) =>
+                                    handleOnSelectChange(e.target.value)
+                                }
+                                label="Range"
+                            >
+                                <MenuItem value={7}>Last 7 Days</MenuItem>
+                                <MenuItem value={30}>Last 30 Days</MenuItem>
+                                <MenuItem value={365}>Last Year</MenuItem>
+                                <MenuItem value="total">Total</MenuItem>
+                                <MenuItem value="custom">Custom Range</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
+                </Stack>
+
+                {customRange && (
+                    <>
+                        <Stack
+                            direction={"row"}
+                            spacing={2}
+                            justifyContent="end"
+                        >
+                            <DatePicker
+                                label="Start Date"
+                                value={startDate}
+                                views={["year", "month", "day"]}
+                                onChange={(date) => {
+                                    if (date) setStartDate(date);
+                                }}
+                                renderInput={(props: TextFieldProps) => (
+                                    <TextField {...props} />
+                                )}
+                            />
+
+                            <DatePicker
+                                label="End Date"
+                                value={endDate}
+                                onChange={(date) => {
+                                    if (date) setEndDate(date);
+                                }}
+                                renderInput={(props: TextFieldProps) => (
+                                    <TextField {...props} />
+                                )}
+                            />
+                        </Stack>
+                    </>
+                )}
+
                 {!perAssetListens ? (
                     <CenteredLoading />
                 ) : (
