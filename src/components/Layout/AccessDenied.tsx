b21d52c2f8e4c1963bb924a55d852d29af73c31b
EngineerDev11 <EngineerDev11@outlook.com>
2024-11-10 19:23:17 +0530

    Added back button

--- src/components/Layout/AccessDenied.tsx
+++ src/components/Layout/AccessDenied.tsx
@@ -1,13 +1,29 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import * as React from "react";
 import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AccessDenied = (): ReactElement => {
+
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate("/projects");
+    };
+
     return (
         <Container sx={styles.container}>
             <Typography variant="h4" sx={styles.heading}>
                 Access Denied
             </Typography>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleRedirect}
+                sx={styles.button}
+            >
+                Go to Home
+            </Button>
         </Container>
     );
 };
@@ -21,11 +37,14 @@ const styles = {
         flexDirection: 'column',
         height: '100vh',
         textAlign: 'center',
-        backgroundColor: '#f8f9fa',
+
     },
     heading: {
         fontSize: '3rem',
         color: '#dc3545', // Red color for Access Denied
+    },
+    button: {
+        marginTop: '20px',
     },
 };
 
